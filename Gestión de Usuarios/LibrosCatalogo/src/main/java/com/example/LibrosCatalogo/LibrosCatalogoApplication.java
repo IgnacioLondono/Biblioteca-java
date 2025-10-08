3dcdd0d10107f1ba04d1e6d7a6564c07fc643ab8
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-10-08 17:05:54 -0300

    Add files via upload

---
+++ Gestión de Usuarios/LibrosCatalogo/src/main/java/com/example/LibrosCatalogo/LibrosCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibrosCatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosCatalogoApplication.class, args);
+	}
+
+}
