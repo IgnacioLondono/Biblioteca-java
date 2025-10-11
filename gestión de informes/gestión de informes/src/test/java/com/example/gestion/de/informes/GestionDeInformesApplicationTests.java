21b165033ea7dcaf1776dc8e6aab82f7f02c4c0e
maty <mati.gaetet@duocuc.cl>
2025-10-11 12:32:45 -0300

    Add files via upload
    gestion  informe mas ordenado

---
+++ gestión de informes/gestión de informes/src/test/java/com/example/gestion/de/informes/GestionDeInformesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion.de.informes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeInformesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
